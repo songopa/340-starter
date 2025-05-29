e6fe0c2a48ea004d469eaf87ded91562e0cfdef9
songopa <songopa@prefix.co.tz>
2025-05-29 22:14:22 +0300

    Robust error handling

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,8 +3,6 @@ const express = require("express");
 const router = express.Router();
 const invController = require("../controllers/invController");
 
-console.log("inventoryRoute.js loaded");
-
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
 module.exports = router;
